5e416bb9df875ee75231dbfe790bc8b248daae36
Omar Hossam El-Din <dummy3@gmail.com>
2021-02-06 12:55:54 +0200

    add-report-policiy

---
+++ Medical-Complex-Services-Backend/app/Policies/ReportPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Report;
+use App\Models\SystemWorker;
+
+class ReportPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @return mixed
+     */
+    public function viewAny(SystemWorker $systemWorker)
+    {
+        return $systemWorker->hasAccess('index-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @param  \App\Models\Report  $report
+     * @return mixed
+     */
+    public function view(SystemWorker $systemWorker, Report $report)
+    {
+        return $systemWorker->hasAccess('show-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @return mixed
+     */
+    public function create(SystemWorker $systemWorker)
+    {
+        return $systemWorker->hasAccess('create-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @param  \App\Models\Report  $report
+     * @return mixed
+     */
+    public function update(SystemWorker $systemWorker, Report $report)
+    {
+        return $systemWorker->hasAccess('update-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @param  \App\Models\Report  $report
+     * @return mixed
+     */
+    public function delete(SystemWorker $systemWorker, Report $report)
+    {
+        return $systemWorker->hasAccess('delete-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @param  \App\Models\Report  $report
+     * @return mixed
+     */
+    public function restore(SystemWorker $systemWorker, Report $report)
+    {
+        return $systemWorker->hasAccess('restore-report') || $systemWorker->hasAccess('control');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\SystemWorker  $systemWorker
+     * @param  \App\Models\Report  $report
+     * @return mixed
+     */
+    public function forceDelete(SystemWorker $systemWorker, Report $report)
+    {
+        return $systemWorker->hasAccess('Force-delete-report') || $systemWorker->hasAccess('control');
+    }
+}
